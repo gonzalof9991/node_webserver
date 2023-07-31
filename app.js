28d1d9e8e9feda74ae241e2c8248e9a380beb16c
gonzalo9991r <go.rojasf@duocuc.cl>
2023-07-31 17:46:35 -0400

    ADD: new changes to package.json

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 require('dotenv').config();
 const port = process.env.PORT;
+
 const hbs = require('hbs');
 // ----------------- Handlebars -----------------
 // Express HBS engine
